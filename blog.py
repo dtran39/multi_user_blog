eaca253fb2a65b272a3368104c2a9a41572a6c61
dtran39 <trananhduc1004@gmail.com>
2016-12-24 06:02:11 +0700

    Add welcome page

--- blog.py
+++ blog.py
@@ -6,31 +6,35 @@ import webapp2
 import jinja2
 
 from google.appengine.ext import db
-
+# Global variables:
+# String full path of the template directory
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+# Jinja environment (load template directory to jinja)
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                autoescape = True)
-def render_str(template, **params):
-    t = jinja_env.get_template(template)
-    return t.render(params)
+# Base Handler
 class BaseHandler(webapp2.RequestHandler):
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
     def render(self, template, **kw):
-        self.response.out.write(render_str(template, **kw))
+        self.response.out.write(self.render_str(template, **kw))
 
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
-
-class Signup(BaseHandler):
-
-    def get(self):
-        self.render("signup-form.html")
-
+# Hello world main page
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello, World!')
-
+class Signup(BaseHandler):
+    def get(self):
+        self.render("signup-form.html")
+class Welcome(BaseHandler):
+    def get(self):
+        self.render("welcome.html", username = "trananhduc1004")
 app = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/signup', Signup)
+    ('/signup', Signup),
+    ('/welcome', Welcome)
 ], debug=True)
