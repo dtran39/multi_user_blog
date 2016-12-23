6481c87728edae9af93a72ed4d10fba6748e77f1
dtran39 <trananhduc1004@gmail.com>
2016-12-23 15:41:28 +0700

    Have sign up running

---
+++ blog.py
@@ -0,0 +1,36 @@
+import os
+import re
+from string import letters
+
+import webapp2
+import jinja2
+
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                               autoescape = True)
+def render_str(template, **params):
+    t = jinja_env.get_template(template)
+    return t.render(params)
+class BaseHandler(webapp2.RequestHandler):
+    def render(self, template, **kw):
+        self.response.out.write(render_str(template, **kw))
+
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+class Signup(BaseHandler):
+
+    def get(self):
+        self.render("signup-form.html")
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Hello, World!')
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+    ('/signup', Signup)
+], debug=True)
