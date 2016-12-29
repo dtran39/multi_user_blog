c0b4f68c7e5e6de9db89549473fee6efa9096f51
dtran39 <trananhduc1004@gmail.com>
2016-12-29 18:29:28 +0700

    Adding comment

--- pages/post.py
+++ pages/post.py
@@ -1,5 +1,5 @@
 from google.appengine.ext import db
-from all_models import PostModel
+from all_models import PostModel, Comment
 from global_helpers import jinja_render_str, blog_key
 from base_render import BlogHandler
 
@@ -13,8 +13,34 @@ class PostPage(BlogHandler):
         key = db.Key.from_path('Post', int(post_id), parent=blog_key())
         post = db.get(key)
 
+        comments = db.GqlQuery("select * from Comment where post_id = " +
+                               post_id + " order by created desc")
         if not post:
             self.error(404)
             return
+        error = self.request.get('error')
+        self.render("permalink.html", post = post, comments = comments, error = error)
+    def post(self, post_id):
+        key = db.Key.from_path('Post', int(post_id), parent=blog_key())
+        post = db.get(key)
 
-        self.render("permalink.html", post = post)
+        if not post:
+            self.error(404)
+            return
+        c = ""
+        if(self.user):
+            # On commenting, it creates new comment tuple
+            if(self.request.get('comment')):
+                c = Comment(parent=blog_key(), user_id=self.user.key().id(),
+                            post_id=int(post_id),
+                            comment=self.request.get('comment'))
+                c.put()
+        else:
+            self.redirect("/login?error=You need to login before " +
+                          "performing edit, like or commenting.!!")
+            return
+
+        comments = db.GqlQuery("select * from Comment where post_id = " +
+                               post_id + "order by created desc")
+
+        self.render("permalink.html", post=post, comments = comments, new=c)
