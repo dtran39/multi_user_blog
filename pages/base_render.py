effacbf22b78dda87e44f40badc285c1c79f0197
dtran39 <trananhduc1004@gmail.com>
2017-01-02 20:11:48 +0700

    Checkstyle for rendering pages

--- pages/base_render.py
+++ pages/base_render.py
@@ -1,35 +1,41 @@
+"""This module has BlogHandler class"""
 import webapp2
-from global_helpers import jinja_render_str, make_secure_val, check_secure_val
+from pages.global_helpers import jinja_render_str, make_secure_val, check_secure_val
 from all_models import User
 # Base blog handler
 class BlogHandler(webapp2.RequestHandler):
-    def write(self, *a, **kw):
-        self.response.out.write(*a, **kw)
-
+    """This class use jinja to render pages"""
     def render_str(self, template, **params):
+        """This method render a string with user"""
         params['user'] = self.user
         return jinja_render_str(template, **params)
 
     def render(self, template, **kw):
-        self.write(self.render_str(template, **kw))
+        """This method write the rendered page"""
+        self.response.out.write(self.render_str(template, **kw))
 
     def set_secure_cookie(self, name, val):
+        """This method set the cookie"""
         cookie_val = make_secure_val(val)
         self.response.headers.add_header(
             'Set-Cookie',
             '%s=%s; Path=/' % (name, cookie_val))
 
     def read_secure_cookie(self, name):
+        """This method double check the cookies"""
         cookie_val = self.request.cookies.get(name)
         return cookie_val and check_secure_val(cookie_val)
 
     def login(self, user):
+        """This method implement login"""
         self.set_secure_cookie('user_id', str(user.key().id()))
 
     def logout(self):
+        """This method implements logout"""
         self.response.headers.add_header('Set-Cookie', 'user_id=; Path=/')
 
     def initialize(self, *a, **kw):
+        """This method initialize handling"""
         webapp2.RequestHandler.initialize(self, *a, **kw)
         uid = self.read_secure_cookie('user_id')
         self.user = uid and User.by_id(int(uid))
--- pages/global_helpers.py
+++ pages/global_helpers.py
@@ -1,24 +1,29 @@
+"""This module has helper functions for other modules"""
 import os
-import jinja2
 import hmac
+import jinja2
 from google.appengine.ext import db
 
-template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
-                               autoescape=True)
 def jinja_render_str(template, **params):
-    t = jinja_env.get_template(template)
-    return t.render(params)
+    """This module use jinja to render string"""
+    template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+    jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
+                                   autoescape=True)
+    template = jinja_env.get_template(template)
+    return template.render(params)
 
 
 def blog_key(name='default'):
+    """this function get the blog key"""
     return db.Key.from_path('blogs', name)
 
 def make_secure_val(val):
+    """This module make a hash from a string"""
     secret = 'duc_tran'
     return '%s|%s' % (val, hmac.new(secret, val).hexdigest())
 
 def check_secure_val(secure_val):
+    """This function check if the hashed value matches"""
     val = secure_val.split('|')[0]
     if secure_val == make_secure_val(val):
         return val
