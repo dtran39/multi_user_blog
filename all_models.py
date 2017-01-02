8977105adf90defd59c0051225ca5a0861bc72f2
dtran39 <trananhduc1004@gmail.com>
2017-01-02 11:05:45 +0700

    Adding like, adding styles to post

--- all_models.py
+++ all_models.py
@@ -1,3 +1,4 @@
 from models.user import User
 from models.post import PostModel
 from models.comment import Comment
+from models.like import Like
--- pages/post.py
+++ pages/post.py
@@ -1,5 +1,5 @@
 from google.appengine.ext import db
-from all_models import PostModel, Comment
+from all_models import PostModel, Comment, Like
 from global_helpers import jinja_render_str, blog_key
 from base_render import BlogHandler
 
@@ -13,13 +13,15 @@ class PostPage(BlogHandler):
         key = db.Key.from_path('Post', int(post_id), parent=blog_key())
         post = db.get(key)
 
+        likes = db.GqlQuery("select * from Like where post_id="+post_id)
         comments = db.GqlQuery("select * from Comment where post_id = " +
                                post_id + " order by created desc")
         if not post:
             self.error(404)
             return
         error = self.request.get('error')
-        self.render("permalink.html", post = post, comments = comments, error = error)
+        self.render("permalink.html", post = post, numOfLikes = likes.count(),
+            comments = comments, error = error)
     def post(self, post_id):
         key = db.Key.from_path('Post', int(post_id), parent=blog_key())
         post = db.get(key)
@@ -29,6 +31,22 @@ class PostPage(BlogHandler):
             return
         c = ""
         if(self.user):
+            # On clicking like, post-like value increases.
+            if(self.request.get('like') and
+               self.request.get('like') == "update"):
+                likes = db.GqlQuery("select * from Like where post_id = " +
+                                    post_id + " and user_id = " +
+                                    str(self.user.key().id()))
+
+                if self.user.key().id() == post.user_id:
+                    self.redirect("/blog/" + post_id +
+                                  "?error=You cannot like your " +
+                                  "post.!!")
+                    return
+                elif likes.count() == 0:
+                    l = Like(parent=blog_key(), user_id=self.user.key().id(),
+                             post_id=int(post_id))
+                    l.put()
             # On commenting, it creates new comment tuple
             if(self.request.get('comment')):
                 c = Comment(parent=blog_key(), user_id=self.user.key().id(),
@@ -39,8 +57,8 @@ class PostPage(BlogHandler):
             self.redirect("/login?error=You need to login before " +
                           "performing edit, like or commenting.!!")
             return
-
+        likes = db.GqlQuery("select * from Like where post_id="+post_id)
         comments = db.GqlQuery("select * from Comment where post_id = " +
                                post_id + "order by created desc")
 
-        self.render("permalink.html", post=post, comments = comments, new=c)
+        self.render("permalink.html", post=post, numOfLikes = likes.count(), comments = comments, new = c)
