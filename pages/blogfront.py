735435b941fe14fc933c8d17c900a6b458d52ed3
dtran39 <trananhduc1004@gmail.com>
2017-01-02 17:53:07 +0700

    Styling on comments and posts

--- pages/blogfront.py
+++ pages/blogfront.py
@@ -3,5 +3,6 @@ from post import Post
 
 class BlogFront(BlogHandler):
     def get(self):
+        deleted_post_id = self.request.get('deleted_post_id')
         posts = greetings = Post.all().order('-created')
-        self.render('front.html', posts = posts)
+        self.render('front.html', posts = posts, deleted_post_id=deleted_post_id)
