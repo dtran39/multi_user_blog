6e521e91a9a22e31729efd2b0a826061afc0d223
dtran39 <trananhduc1004@gmail.com>
2017-01-03 14:50:56 +0700

    Deleting post will delete associated likes and comments, removing new comments posted showing because it will be added immediately to db

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import webapp2
 from all_pages import (Register, Login, Logout, BlogFront, PostPage,
                        NewPost, DeletePost, EditPost, DeleteComment, EditComment)
 app = webapp2.WSGIApplication([
-    ('/signup', Register),
-    ('/login', Login),
-    ('/logout', Logout),
     ('/?', BlogFront),
     ('/blog/([0-9]+)', PostPage),
     ('/blog/new', NewPost),
     ('/blog/edit/([0-9]+)', EditPost),
     ('/blog/delete/([0-9]+)', DeletePost),
     ('/blog/deletecomment/([0-9]+)/([0-9]+)', DeleteComment),
-    ('/blog/editcomment/([0-9]+)/([0-9]+)', EditComment)
+    ('/blog/editcomment/([0-9]+)/([0-9]+)', EditComment),
+    ('/signup', Register),
+    ('/login', Login),
+    ('/logout', Logout)    
     ], debug=True)
--- pages/deletepost.py
+++ pages/deletepost.py
@@ -11,6 +11,13 @@ class DeletePost(BlogHandler):
             post = db.get(key)
             if post.user_id == self.user.key().id():
                 post.delete()
+                # Remember to delete comments and likes
+                likes = db.GqlQuery("select * from Like where post_id="+post_id)
+                for a_like in likes:
+                    a_like.delete()
+                comments = db.GqlQuery("select * from Comment where post_id=" + post_id)
+                for a_comment in comments:
+                    a_comment.delete()
                 self.redirect("/?deleted_post_id="+post_id)
             else:
                 self.redirect("/blog/" + post_id + "?error=You don't have " +
